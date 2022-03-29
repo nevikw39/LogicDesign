b73f2db829b8c8bda551b5d5e654ef43ba670ba6
nevikw39 <nevikw39@m110.nthu.edu.tw>
2022-03-29 21:12:26 +0800

    lab1: Gate-level Description

--- lab1/src/Fib.v
+++ lab1/src/Fib.v
@@ -19,3 +19,24 @@ module happy_verilog_B(i, o);
     end
 
 endmodule
+
+
+module happy_verilog_G(i, o);
+
+    parameter n = 4;
+
+    input[n - 1 : 0] i;
+    output o;
+
+    wire not_a, not_b, not_c, not_d, and_0, and_1, and_2;
+
+    not(not_a, i[3]);
+    not(not_b, i[2]);
+    not(not_c, i[1]);
+    not(not_d, i[0]);
+    and(and_0, not_a, not_b);
+    and(and_1, i[2], not_c, i[0]);
+    and(and_2, not_b, not_c, not_d);
+    or(o, and_0, and_1, and_2);
+
+endmodule
--- lab1/tests/Fib_test.v
+++ lab1/tests/Fib_test.v
@@ -19,20 +19,21 @@ module happy_verilog_tb;
     parameter n = 4, delay = 5;
 
     reg[n - 1 : 0] i;
-    wire o_b;
+    wire o_b, o_g;
 
     happy_verilog_B hvb(i, o_b);
+    happy_verilog_G hvg(i, o_g);
 
-    wire helper_b;
+    wire helper_b, helper_g;
 
-    happy_verilog_tb_helper hvb_helper(o_b, i, helper_b);
+    happy_verilog_tb_helper hvb_helper(o_b, i, helper_b), hvg_helper(o_g, i, helper_g);
 
     initial begin
         i = 1'b0;
         repeat (16) begin
             #delay;
-            $display("time = %4d, i = %b, o_b = %b", $time, i, o_b);
-            if (helper_b) begin
+            $display("time = %4d, i = %b, o_b = %b, o_g = %b", $time, i, o_b, i, o_g);
+            if (helper_b || helper_g) begin
                 $display("WA");
                 $finish;
             end
