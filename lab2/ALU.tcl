2a02f8f7d2c30a32e64cf5e6c9bd87cf6ad9165b
nevikw39 <nevikw39@m110.nthu.edu.tw>
2022-04-25 00:57:18 +0800

    Update path of src

--- lab2/ALU.tcl
+++ lab2/ALU.tcl
@@ -1,8 +1,8 @@
-read_file -format verilog {./ALU.v}
+read_file -format verilog {src/ALU.v}
 Current design is 'ALU'
-analyze -library WORK -format verilog {./ALU.v}
+analyze -library WORK -format verilog {src/ALU.v}
 elaborate ALU -architecture verilog -library WORK
 compile -exact_map
-write -hierarchy -format verilog -output ./ALU_syn.v
+write -hierarchy -format verilog -output src/ALU_syn.v
 write_sdf -version 1.0 -context verilog ALU.sdf
 exit
